08ef0d7bde5cbf3d81e7125cd269ac5392e923e0
dabbl <dabbler503@gmail.com>
2025-03-18 19:26:53 +0500

    add 8 9

--- src/index.js
+++ src/index.js
@@ -21,10 +21,17 @@ function isDog(card) {
     return card instanceof Dog;
 }
 
+function isLad(card) {
+    return card instanceof Lad && Lad.prototype.hasOwnProperty('modifyDealedDamageToCreature') && Lad.prototype.hasOwnProperty('modifyTakenDamage')
+}
+
 // Дает описание существа по схожести с утками и собаками
 function getCreatureDescription(card) {
     if (isDuck(card) && isDog(card)) {
         return 'Утка-Собака';
+    }
+    if (isLad(card)) {
+        return 'Чем их больше, тем они сильнее'
     }
     if (isDuck(card)) {
         return 'Утка';
@@ -62,8 +69,8 @@ class Trasher extends Dog{
         super(name,maxPower,image);
     }
     modifyTakenDamage(value, fromCard, gameContext, continuation){
-        this.view.signalAbility(() => super.takeDamage(value - 1,fromCard,gameContext,continuation));
-        continuation(value);
+        this.view.signalAbility(()=> {this.view.signalDamage(continuation(0))});
+        continuation(value - 1);
     }
     getDescription(){
         return [
@@ -78,32 +85,107 @@ class Gatling extends Creature{
 
     attack(gameContext, continuation) {
         const taskQueue = new TaskQueue();
+
         const {currentPlayer, oppositePlayer, position, updateView} = gameContext;
-        for (let i = 0; i < oppositePlayer.table.length; i++) {
-            taskQueue.push(onDone => this.view.showAttack(onDone));
+
+        taskQueue.push(onDone => this.view.showAttack(onDone));
+        for (let card of oppositePlayer.table) {
             taskQueue.push(onDone => {
-                const oppositeCard = oppositePlayer.table[i];
-                if (oppositeCard) {
-                    this.dealDamageToCreature(2, oppositeCard, gameContext, onDone);
-                }
+                this.dealDamageToCreature(2, card, gameContext, onDone);
             });
         }
-        taskQueue.continueWith(continuation)
+
+        taskQueue.continueWith(continuation);
     }
+}
+
+class Lad extends Dog{
+    constructor(name = "Браток", power = 2) {
+        super(name, 2);
+    }
+
+    static getInGameCount() {
+        return this.inGameCount || 0;
+    }
+
+    static setInGameCount(value) {
+        this.inGameCount = value;
+    }
+
+    doAfterComingIntoPlay(gameContext, continuation) {
+        const {currentPlayer, oppositePlayer, position, updateView} = gameContext;
+        Lad.setInGameCount(Lad.inGameCount + 1);
+        continuation();
+    };
+
+    doBeforeRemoving(continuation) {
+        Lad.setInGameCount(Lad.inGameCount - 1);
+        continuation();
+    };
+
+    modifyDealedDamageToCreature(value, toCard, gameContext, continuation) {
+        continuation(value + Lad.getBonus());
+    };
+
+    modifyTakenDamage (value, fromCard, gameContext, continuation) {
+        continuation(value - Lad.getBonus());
+    };
+
+    static getBonus() {
+        return this.getInGameCount() * (this.getInGameCount() + 1) / 2;
+    }
+}
+
+class PseudoDuck extends Dog{
+    constructor(name = "Псевдоутка", power = 3) {
+        super(name, power);
+    }
+
+    quacks() {
+        console.log('quack')
+    };
+
+    swims() {
+        console.log('float:both;')
+    };
+}
+
+class Brewer extends Duck{
+    constructor(name = "Пивовар", power = 2, image = 'krasnopuzoff-sticker.png') {
+        super(name, power, image);
+    }
+
+    attack(gameContext, continuation) {
+        const allCards = gameContext.currentPlayer.table.concat(gameContext.oppositePlayer.table);
+        allCards.forEach(card => {
+            if (isDuck(card)) {
+                card.maxPower = card.maxPower + 1;
+                card.currentPower = card.currentPower + 2;
+                card.view.signalHeal(() => {
+                    card.updateView();
+                });
+            }
+        });
+        super.attack(gameContext, continuation);
+    }
+
 }
 
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Duck(),
-    new Duck(),
-    new Duck(),
+    new PseudoDuck(),
+    new Brewer(),
 ];
 
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
     new Trasher(),
+    new Lad(),
+    new Lad(),
+    new Lad(),
+    new Lad(),
 ];
 
 
